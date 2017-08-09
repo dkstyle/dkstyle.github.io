6035a5cdf3587115c7bea56f20fd4eca54468c34
Dongkyun Yoo <dkyoo@sk.com>
2017-08-09 13:08:17 +0900

    webslide

---
+++ SEC_POC/webslide/slide_config.js
@@ -0,0 +1,36 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'SEC PoC Demo<br/> ',
+    subtitle: 'Data Visualization',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    //favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    //name: 'Dongkyun Yoo',
+    company: 'IBM Korea',
+    gplus: 'http://plus.google.com/dkstyle',
+    twitter: '@dkstyle1',
+    www: 'http://www.ibm.com',
+    github: 'http://github.com/dkstyle'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
